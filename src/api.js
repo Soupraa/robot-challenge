798bcf9318ab3ea859b5d95eb413f75ccbecaf9e
Richard Gao <49212960+Soupraa@users.noreply.github.com>
2021-05-29 20:32:02 +1000

    linked client and api
    basic post and get method in client

--- src/api.js
+++ src/api.js
@@ -3,12 +3,28 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 
-
+var string = '';
 app.use(cors());
+app.use(express.json({limit: '1mb'}));
 
 app.get('/', (req, res) => {
-    res.json({foo: 'bar'});
+    res.send("API is working properly");
+});
+app.post('/api', (request, response) => {
+    console.log("request received!")
+    const data = request.body;
+    // console.log(data.x);
+    response.json({
+        status: 'success',
+        instructions: data.x,
+    });
+    string = data.x;
+    console.log(string);
 });
 
+app.get('/api/answer', (req, res) => {
+    console.log("Getting Data!");
+    console.log(string);
+    res.send(string);
+});
 app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
-
--- src/client.js
+++ src/client.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import useFetch from 'react-fetch-hook';
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
+import useFetch from "react-fetch-hook";
 
+/*method to send data to backend API*/
+function handleSubmit() { 
+  const x = document.getElementById("instructions").value;
+  const data ={x};
 
-function App() {
-    const {isLoading, data} = useFetch('http://localhost:4001');
-    if(isLoading) {
-        return 'Loading...';
-    }
-    return <pre>{JSON.stringify(data, null, 4)}</pre>;
+  const s = JSON.stringify(x);
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(data)
+    };
+    fetch('http://localhost:4001/api', options).then(response => {
+        console.log(response);
+    });
 }
 
+/*tmp method to retreive data from backend API*/
+function getData(){
+   axios.get('http://localhost:4001/api/answer').then(function(response){
+       console.log("Client log");
+       console.log(response.data);
+   });
+}
+function App() {
+  // const {isLoading, data} = useFetch('http://localhost:4001');
+  // if(isLoading) {
+  //     return 'Loading...';
+  // }
+  return (
+    <div>
+      <h2>Robot Challenge</h2>
+      <form classname="form" id="myForm" onSubmit={handleSubmit}>
+        <label>Instructions:</label>
+        <input type="text" name="instructions" id="instructions" />
+        <button type="submit" onclick="handleSubmit()">
+          submit
+        </button>
+       
+      </form>
+    <button onClick={getData}>click me</button>
+      {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
+    </div>
+  );
+}
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
